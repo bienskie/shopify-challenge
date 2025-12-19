0436809edcc35c9b1e9f5b87907201c90e0fc121
Bien Relampagos <bien.relampagos@august99.com>
2025-12-19 15:04:25 +0800

    for header

---
+++ assets/js/global.js
@@ -0,0 +1,23 @@
+(function ($, w, d, h, b) {
+
+    var app = {
+
+        custom_script: function () {
+            
+        },
+
+    }
+
+
+    jQuery(document).ready(function () {
+        app.custom_script();;
+
+    });
+
+    jQuery(window).on('load', function () {
+
+        
+    })
+
+
+})(jQuery, window, document, 'html', 'body');
